f09c5f7f8805bdf8453e3251650052b55792a183
unknown <bosu15-5645@diu.edu.bd>
2019-04-25 02:23:00 +0600

    C# Loop Example

---
+++ loop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[] numbers = new int[size];
+            for (int i = 1; i <= size; i++)
+            {
+                numbers[i - 1] = i;
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                int anumber = numbers[i];
+                if (anumber % 2 == 0)
+                    
+                {
+                    
+                    //Console.Write("even=" );
+                    Console.WriteLine(anumber);
+                    
+                } 
+
+                else
+                   
+                {
+                    Console.Write("odd= ");
+                    Console.WriteLine(anumber);
+            
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
